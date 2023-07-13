221da6e9d21e0afff7db16bb6384e78c20146f35
Wilson Muita Wanjiru <wilsonmuita41@gmail.com>
2023-07-13 12:56:03 +0300

    <Stitching Together the Three Pillars>

--- test/helpers.js
+++ test/helpers.js
@@ -1,11 +1,11 @@
-const chai = require('chai')
-global.expect = chai.expect
-const fs = require('fs')
-const jsdom = require('mocha-jsdom')
-const path = require('path')
+const chai = require('chai');
+global.expect = chai.expect;
+const fs = require('fs');
+const jsdom = require('mocha-jsdom');
+const path = require('path');
 const babel = require('babel-core');
 
-const html = fs.readFileSync(path.resolve(__dirname, '..', 'index.html'), 'utf-8')
+const html = fs.readFileSync(path.resolve(__dirname, '..', 'index.html'), 'utf-8');
 
 const babelResult = babel.transformFileSync(
   path.resolve(__dirname, '..', 'index.js'), {
@@ -13,8 +13,9 @@ const babelResult = babel.transformFileSync(
   }
 );
 
-const src = babelResult.code
+const src = babelResult.code;
 
 jsdom({
-  html, src
+  html,
+  src
 });
--- test/indexTest.js
+++ test/indexTest.js
@@ -1,6 +1,6 @@
 describe("index.js", () => {
   it("exists", () => {
-    expect(testVar).to.exist
-    expect(testVar).to.deep.equal({}) //without .deep, {} does not equal {}
-  })
-})
+    expect(testVar).to.exist;
+    expect(testVar).to.deep.equal({}); //without .deep, {} does not equal {}
+  });
+});
